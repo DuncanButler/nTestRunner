b1c62d554418e10fd6e0a2b4fba7ff57b3df2657
Duncan <duncan.butler1@btinternet.com>
2011-02-05 01:17:18 +0000

    refactor configuration and program

---
+++ nTestRunner/Configuration.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace nTestRunner
+{
+    public class Configuration
+    {
+        public Configuration(string[] args)
+        {
+            SetDefaultValues();
+
+            SetArgumentValues(args);
+        }
+
+        void SetDefaultValues()
+        {
+            SolutionPath = SetWatchFilePath();
+            TestRunner = string.Empty;
+            ResultDisplay = string.Empty;
+        }
+
+        void SetArgumentValues(string[] args)
+        {
+            if (args.Length == 0)
+                return;
+
+            for (int i = 0; i < args.Length -1; i+=2)
+            {
+                string key = args[i];
+                string value = args[i + 1];
+
+                if (key.ToLower() == "-path" || key.ToLower() == "-p")
+                    SolutionPath = value;
+
+                if (key.ToLower() == "-test" || key.ToLower() == "-t")
+                    TestRunner = value;
+
+                if (key.ToLower() == "-display" || key.ToLower() == "-d")
+                    ResultDisplay = value;
+            }
+        }
+
+        string SetWatchFilePath()
+        {
+            DirectoryInfo searchDirectory = GetSearchDirectory();
+
+            var files = GetSolutionOrProjectFiles(searchDirectory);
+
+            return GetWatchFilePath(files);
+        }
+
+        DirectoryInfo GetSearchDirectory()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+
+            return Directory.GetParent(path).Parent;
+        }
+
+        IEnumerable<FileInfo> GetSolutionOrProjectFiles(DirectoryInfo searchDirectory)
+        {
+            var files = GetFilesWithPattern(searchDirectory, "*.sln");
+
+            if (FoundFiles(files))
+                return files;
+
+            return GetFilesWithPattern(searchDirectory, "*.csproj");
+        }
+
+        FileInfo[] GetFilesWithPattern(DirectoryInfo searchDirectory, string searchPattern)
+        {
+            return searchDirectory.GetFiles(searchPattern, SearchOption.TopDirectoryOnly);
+        }
+
+        bool FoundFiles(FileInfo[] files)
+        {
+            return files.Length > 0;
+        }
+
+        string GetWatchFilePath(IEnumerable<FileInfo> files)
+        {
+            return files.FirstOrDefault().FullName;
+        }
+
+        public string TestRunner { get; private set; }
+        public string SolutionPath { get; private set; }
+        public string ResultDisplay { get; private set; }
+    }
+}
